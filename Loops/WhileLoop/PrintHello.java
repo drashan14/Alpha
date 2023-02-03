5a9b398bf19480a611b5d0d85c36d1441fe7550a
Darshan Balaji Rathod <90858956+drashan14@users.noreply.github.com>
2023-02-03 22:51:41 +0530

    Loops FIle

---
+++ Loops/WhileLoop/PrintHello.java
@@ -0,0 +1,13 @@
+package Loops.WhileLoop;
+
+public class PrintHello {
+    public static void main(String args[]) {
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("HEllo");
+            counter++;
+
+        }
+    }
+
+}
